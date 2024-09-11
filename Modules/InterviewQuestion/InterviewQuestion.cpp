fcebc9659afecedfdee5304918c227100e2655b3
babuge <987321144@qq.com>
2024-09-11 21:15:46 +0800

    interview question 3

--- Modules/InterviewQuestion/InterviewQuestion.cpp
+++ Modules/InterviewQuestion/InterviewQuestion.cpp
@@ -1,5 +1,72 @@
 #include "InterviewQuestion.h"
 
-InterviewQuestion::InterviewQuestion()
+InterviewQuestion::InterviewQuestion() {}
+
+int InterviewQuestion::GetSecond(const QVector<int> &vct) const
 {
+    int second = std::numeric_limits<int>::min();
+    int max    = std::numeric_limits<int>::min();
+
+    for (int i = 0; i < vct.size(); ++i) {
+        if (vct.at(i) >= max) {
+            second = max;
+            max    = vct.at(i);
+        }
+        if (vct.at(i) > second && vct.at(i) < max) {
+            second = vct.at(i);
+        }
+    }
+    return second;
+}
+
+int InterviewQuestion::FindSubSetIndex(const QVector<int> &vct, const QVector<int> &vctSub) const
+{
+    int index = -1;
+    if (vctSub.size() == 0) {
+        return 0;
+    }
+    if (vct.size() < vctSub.size()) {
+        return index;
+    }
+    for (int i = 0; i < vct.size(); ++i) {
+        for (int j = 0; j < vctSub.size(); ++j) {
+            if (index == -1) {
+                if (j == 0 && vct.at(i) == vctSub.at(j)) {
+                    index = i;
+                }
+            }
+            else {
+                if ((i + j) >= vct.size() && j != (vctSub.size() - 1)) {
+                    index = -1;
+                    return index;
+                }
+                if (vct.at(i + j) != vctSub.at(j)) {
+                    index = -1;
+                    break;
+                }
+                else if (j == vctSub.size() - 1) {
+                    return index;
+                }
+            }
+        }
+    }
+    return index;
+}
+
+std::string InterviewQuestion::CycleString(std::string str, int s32Step) const
+{
+    int len = str.size();
+    if (len <= 1) {
+        return str;
+    }
+    s32Step = s32Step / len;
+    if (s32Step <= 0) {
+        return str;
+    }
+    char buff[len + 1];
+    memset(buff, '\0', sizeof(buff));
+    str.copy(buff, len - s32Step, s32Step);
+    str.copy(&buff[len - s32Step], s32Step);
+    str = std::string(buff);
+    return str;
 }
--- Modules/InterviewQuestion/InterviewQuestion.h
+++ Modules/InterviewQuestion/InterviewQuestion.h
@@ -2,8 +2,16 @@
 
 #include "InterviewQuestion_global.h"
 
+#include <QVector>
+
 class INTERVIEWQUESTION_EXPORT InterviewQuestion
 {
 public:
     InterviewQuestion();
+
+    int GetSecond(const QVector<int> &vct) const;
+
+    int FindSubSetIndex(const QVector<int> &vct, const QVector<int> &vctSub) const;
+
+    bool CycleString(std::string str, int s32Step) const;
 };
